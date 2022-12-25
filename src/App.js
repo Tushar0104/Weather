60928ce5d7b08d4e4417998fb83e32f5fdb9b67c
Tushar Agrawal <tusharagrawal0104@gmail.com>
2022-12-25 15:53:14 +0530

    frontend commit changes2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
-import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
@@ -10,7 +9,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/home" component={Home} />
         </Switch>
       </Router>
     </div>
